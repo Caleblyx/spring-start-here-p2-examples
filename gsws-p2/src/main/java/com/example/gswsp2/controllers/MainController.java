e1f5fd3e016e096c64fe7816e72a514d3587823d
Caleb <caleblyx@outlook.com>
2024-03-31 19:32:06 +0800

    make color param optional

--- gsws-p2/src/main/java/com/example/gswsp2/controllers/MainController.java
+++ gsws-p2/src/main/java/com/example/gswsp2/controllers/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
     @RequestMapping("/home")
     public String home(
-        @RequestParam String color,
+        @RequestParam(required = true) String color,
         Model page) {
         page.addAttribute("username", "Katy");
         page.addAttribute("color", color);
