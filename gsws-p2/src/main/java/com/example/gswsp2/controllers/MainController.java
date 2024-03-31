6777b03df9b3869658c2a4a954879b39e8d7a719
Caleb <caleblyx@outlook.com>
2024-03-31 19:26:13 +0800

    complete exercise

--- gsws-p2/src/main/java/com/example/gswsp2/controllers/MainController.java
+++ gsws-p2/src/main/java/com/example/gswsp2/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.example.gswsp2.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model page) {
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", "red");
         return "home.html";
     }
     
