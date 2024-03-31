e357b7b0653a3e899453bbb1dd29f1ac9668a5a5
Caleb <caleblyx@outlook.com>
2024-03-31 16:18:35 +0800

    initialize spring boot project

---
+++ gsws-p2/src/main/java/com/example/gswsp2/GswsP2Application.java
@@ -0,0 +1,13 @@
+package com.example.gswsp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GswsP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GswsP2Application.class, args);
+	}
+
+}
---
+++ gsws-p2/src/test/java/com/example/gswsp2/GswsP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gswsp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GswsP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
