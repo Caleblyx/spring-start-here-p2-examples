2134a57308411435856f29d1e8013f14d19c3140
Caleb <caleblyx@outlook.com>
2024-04-03 21:16:02 +0800

    complete exercise

--- gsws-p2/src/main/java/com/example/gswsp2/controllers/MainController.java
+++ gsws-p2/src/main/java/com/example/gswsp2/controllers/MainController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.gswsp2.services.LoggedUserManagementService;
+import com.example.gswsp2.services.LoginCountService;
 
 
 @Controller
 public class MainController {
     
     LoggedUserManagementService loggedUserManagementService;
+    LoginCountService loginCountService;
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
@@ -24,12 +27,14 @@ public class MainController {
         }
 
         String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
 
         if (username == null) {
             return "redirect:/";
         }
 
         model.addAttribute("username", username);
+        model.addAttribute("loginCount", count);
         return "main.html";
     }
     
--- gsws-p2/src/main/java/com/example/gswsp2/processors/LoginProcessor.java
+++ gsws-p2/src/main/java/com/example/gswsp2/processors/LoginProcessor.java
@@ -5,21 +5,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import com.example.gswsp2.services.LoggedUserManagementService;
+import com.example.gswsp2.services.LoginCountService;
 
 @Component
 @RequestScope
 public class LoginProcessor {
     
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     public boolean login() {
+        loginCountService.increment();
+
         String username = this.getUsername();
         String password = this.getPassword();
 
