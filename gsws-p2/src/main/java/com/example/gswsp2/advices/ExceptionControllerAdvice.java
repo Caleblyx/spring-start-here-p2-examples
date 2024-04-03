7000d1c4cde45cdd98a5da2a0158986a6e781f8f
Caleb <caleblyx@outlook.com>
2024-04-03 23:16:48 +0800

    fix bug

--- gsws-p2/src/main/java/com/example/gswsp2/advices/ExceptionControllerAdvice.java
+++ gsws-p2/src/main/java/com/example/gswsp2/advices/ExceptionControllerAdvice.java
@@ -4,16 +4,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.gswsp2.exceptions.NotEnoughMoneyException;
 import com.example.gswsp2.model.ErrorDetails;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
     
-    @ExceptionHandler
+    @ExceptionHandler(NotEnoughMoneyException.class)
     public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
         ErrorDetails errorDetails = new ErrorDetails();
 
-        errorDetails.setMessage("Not enough money make the payment");
+        errorDetails.setMessage("Not enough money to make the payment");
 
         return ResponseEntity.badRequest().body(errorDetails);
     }
